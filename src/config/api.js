797f41301f3741c2772589cea0b2761b9f401c26
Vikash Katiyar <rishabktr018@gmail.com>
2023-07-29 12:11:59 +0530

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,17 @@
+
+import axios from 'axios';
+const LOCALHOST='https://clothify-backend-production.up.railway.app/'
+
+export const API_BASE_URL = LOCALHOST
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+const token = localStorage.getItem('jwt');
+
+api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default api;
