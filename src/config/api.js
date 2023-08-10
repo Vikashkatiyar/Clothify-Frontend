2b02bf7b4c3b2ddc11a34ad6bbd57be284986561
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-08-10 20:06:01 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 // const LOCALHOST='https://clothify-backend-production.up.railway.app/'
 
-export const API_BASE_URL = 'https://clothify-backend-production.up.railway.app/'
+export const API_BASE_URL = 'https://clothify-backend-production.up.railway.app'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
